d5140b7e9bbc64e086119f1ae99834721c11bbc0
edsontanahara <edsonmoriaki.s.t@gmail.com>
2026-06-07 17:03:07 -0300

    Delete src/main/java/com/geargest/cliente directory

--- src/main/java/com/geargest/cliente/Cliente.java
+++
@@ -1,5 +0,0 @@
-package com.geargest.cliente;
-
-public class ClienteController {
-    // Aqui vão as rotas/regras para receber os dados da tela de clientes
-}
--- src/main/java/com/geargest/cliente/ClienteController.java
+++
@@ -1,5 +0,0 @@
-package com.geargest.cliente;
-
-public class ClienteService {
-    // Aqui vão as regras de negócio (ex: validar se o CPF do cliente está correto)
-}
